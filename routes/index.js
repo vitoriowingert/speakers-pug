6a724099a43cd5c01c04ea39844f468c220f6a03
Vitorio Wingert <vitoriowingert@hotmail.com>
2024-11-08 09:20:40 -0300

    feat: removed the data load on index router

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-var speakersData = require("../public/data/speakers.json");
-
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
     title: "Speakers test",
-    speakers: speakersData.speakers,
   });
 });
 
